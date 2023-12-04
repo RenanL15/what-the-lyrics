b92b3901a775fa6f69d72844d585236aba31b7a0
Renan <renanalmeida201612@gmail.com>
2023-12-04 01:49:56 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  return (
+    <main className="bg-neutral-900 flex justify-center items-center min-h-screen">
+      <div className="">
+        <h1>What the lyrics</h1>
+      </div>
+    </main>
+  );
+}
+
+export default App;
