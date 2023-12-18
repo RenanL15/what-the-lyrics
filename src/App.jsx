7fdc0944d4ed3e3aaaa4a71a1d2e7257046b9e81
Renan <renanalmeida201612@gmail.com>
2023-12-18 17:51:44 -0300

    style: list, input

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,50 @@
+import { useState } from "react";
 import Songs from "./components/Songs";
 
 function App() {
+  const [inputUrl, setInputUrl] = useState("");
+  const [playlistUrl, setPlaylistUrl] = useState("");
+
   return (
-    <main className="flex items-center justify-center min-h-screen bg-neutral-900">
-      <div className="flex flex-col items-center gap-12">
+    <main className="flex items-center justify-center min-h-screen bg-gray-800">
+      <div className="flex flex-col items-center">
         <div className="flex flex-col items-center">
           <h1 className="text-4xl font-bold text-white">What the lyrics</h1>
-          <input
-            type="text"
-            placeholder="Enter your playlist"
-            className="px-2 py-2 text-2xl w-[30rem] rounded-xl"
-          />
-          <audio controls>
+          <div className="flex items-center my-12 max-w-[60rem] mx-6 gap-2">
+            <div className="flex flex-col">
+              <label htmlFor="" className="text-xl text-white">
+                Enter your playlist
+              </label>
+              <div className="flex items-center">
+                <input
+                  type="text"
+                  value={inputUrl}
+                  onChange={(e) => {
+                    setInputUrl(e.target.value);
+                  }}
+                  size={100}
+                  placeholder="https://open.spotify.com/playlist/34utpZc9RZRShEZso9DV65"
+                  className="w-full px-2 py-2 text-2xl text-white bg-transparent border border-green-600 placeholder:opacity-50"
+                />
+                <button
+                  onClick={() => {
+                    setPlaylistUrl(inputUrl);
+                    setInputUrl("");
+                  }}
+                  className="px-12 py-3 text-xl font-semibold text-white bg-green-700">
+                  Go
+                </button>
+              </div>
+            </div>
+          </div>
+          {/* <audio controls>
             <source
               src="https://p.scdn.co/mp3-preview/eb9de15a4465f504ee0eadba93e7d265ee0ee6ba?cid=e89c58aca13d4e72bc7dc02521952367"
               type="audio/mpeg"
             />
-          </audio>
+          </audio> */}
         </div>
-        <Songs />
+        <Songs playlistUrl={playlistUrl} />
       </div>
     </main>
   );
--- src/components/Songs.jsx
+++ src/components/Songs.jsx
@@ -1,43 +1,92 @@
-import axios from "axios";
 import { useEffect } from "react";
 import { useState } from "react";
+import { dotWave } from "ldrs";
+dotWave.register("l-dot-wave");
+import axios from "axios";
+import qs from "qs";
+import UnknownSong from "../assets/unk-song.jpg";
 
-export default function Songs() {
+export default function Songs({ playlistUrl }) {
   const [playlistData, setPlaylistData] = useState([]);
-
+  const [loading, setLoading] = useState(true);
+  const [noResult, setNoResult] = useState(false);
   useEffect(() => {
+    const URL = playlistUrl.substr(34, playlistUrl.length);
+    setNoResult(false);
+    setLoading(true);
     axios
-      .get(
-        "https://api.spotify.com/v1/playlists/5MVD62fLi3lBrgpHsxtRPE?si=4a95bcd04f7f4eae&limit=1",
-        {
-          headers: {
-            Authorization: `Bearer ${import.meta.env.VITE_TOKEN}`,
-          },
-        }
+      .post(
+        "https://accounts.spotify.com/api/token",
+        qs.stringify({
+          grant_type: "client_credentials",
+          client_id: import.meta.env.VITE_CLIENT_ID,
+          client_secret: import.meta.env.VITE_CLIENT_SECRET,
+        })
       )
       .then((res) => {
         console.log(res);
-        console.log(res.data.tracks.items);
-        setPlaylistData(res.data.tracks.items);
+        axios
+          .get(`https://api.spotify.com/v1/playlists/${URL}`, {
+            headers: {
+              Authorization: `Bearer ${res.data.access_token}`,
+            },
+          })
+          .then((res) => {
+            setLoading(false);
+            setNoResult(false);
+            console.log(res.data.tracks.items);
+            setPlaylistData(res.data.tracks.items);
+
+            res.data.tracks.items.forEach((element) => {
+              for (const key in element.track.album) {
+                const obj = element.track.album[key];
+                console.log(obj);
+              }
+            });
+          })
+          .catch(() => {
+            setLoading(false);
+            setNoResult(true);
+            setPlaylistData([]);
+          });
+      })
+      .catch(() => {
+        setLoading(false);
+        setNoResult(true);
       });
-  }, []);
+  }, [playlistUrl]);
 
   return (
-    <div className="flex flex-wrap justify-center gap-6">
-      {
+    <div className="flex flex-wrap justify-center gap-20 pb-20">
+      {loading ? (
+        <l-dot-wave size={100} color={"rgb(22,163,74)"}></l-dot-wave>
+      ) : (
         //   Array(10)
         //     .fill("")
-        playlistData.map((e) => (
-          <div className="p-4 bg-white h-72 rounded-xl w-60">
+        playlistData.map((e, i) => (
+          <div
+            onClick={() => window.open(e.track.external_urls.spotify)}
+            key={i}
+            className="flex flex-col items-center gap-2 p-4 text-white border border-green-600 cursor-pointer w-60 hover:bg-green-800 hover:bg-opacity-20">
             <img
-              src={`${e.track.album.images[1].url}`}
+              src={
+                e.track.album.images.length < 1
+                  ? UnknownSong
+                  : e.track.album.images[1].url
+              }
               alt="Song picture"
               width={200}
             />
-            <p className="text-xl font-bold text-center">{e.track.name}</p>
+            <p className="text-xl font-semibold text-center">{e.track.name}</p>
+            <p className="text-xl text-center text-green-500">
+              {e.track.artists[0].name}
+            </p>
           </div>
         ))
-      }
+      )}
+      {noResult && (
+        <span className="text-xl text-white">Playlist não encontrada</span>
+      )}
     </div>
   );
 }
